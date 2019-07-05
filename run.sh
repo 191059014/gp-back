7c37f4f6de750cdd56f33aa75b8902a04c771d62
黄彪 <191059014@qq.com>
2019-07-05 14:14:26 +0800

    更新run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ java -server -Xmx4000m -Xms4000m -Xss512k -XX:+DisableExplicitGC \
      -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled \
      -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods \
      -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 \
+
+     cp /var/lib/jenkins/workspace/gp-back/target/gp-back-0.0.1-SNAPSHOT.jar /data/jenkins/deploy
+     cp /var/lib/jenkins/workspace/gp-back/run.sh /data/jenkins/deploy
      nohup java -jar gp-back-0.0.1-SNAPSHOT.jar >nohup.log 2>&1
