83f33de0d753f6b82146892a7a5dd1b37f184180
黄彪 <191059014@qq.com>
2019-07-05 13:56:21 +0800

    增加springboot-maven插件

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ java -server -Xmx4000m -Xms4000m -Xss512k -XX:+DisableExplicitGC \
      -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled \
      -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods \
      -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 \
-     nohup java -jar /var/lib/jenkins/workspace/gp-back/target/gp-back-0.0.1-SNAPSHOT.jar >nohup.log 2>&1
+     nohup java -jar gp-back-0.0.1-SNAPSHOT.jar >nohup.log 2>&1
