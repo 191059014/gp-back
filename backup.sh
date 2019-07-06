d756ef6eda703a04e85a8a3efcaaf3f691c47c15
黄彪 <191059014@qq.com>
2019-07-06 19:35:34 +0800

    增加脚本

--- backup.sh
+++ backup.sh
@@ -2,8 +2,10 @@
 file="/data/jenkins/deploy/gp-back.jar"
 if [[ -f "$file" ]]
 then
-   echo "backup project start..."
+   echo "backup last package start..."
    mv /data/jenkins/deploy/gp-back.jar /data/jenkins/backup/gp-back.jar.`date +%Y%m%d%H%M%S`
-   echo "backup project complete..."
+   echo "backup last package complete..."
 fi
+   echo "backup compile package start..."
    mv /var/lib/jenkins/workspace/gp-back/target/gp-back-*-SNAPSHOT.jar /data/jenkins/backup/gp-back.jar
+   echo "backup compile package complete..."
