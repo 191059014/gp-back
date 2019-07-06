93b450c8d9e17b17d02f1fe757b9a0d09385451e
黄彪 <191059014@qq.com>
2019-07-06 20:06:23 +0800

    增加脚本

--- backup.sh
+++ backup.sh
@@ -6,6 +6,3 @@ then
    mv /data/jenkins/deploy/gp-back.jar /data/jenkins/backup/gp-back.jar.`date +%Y%m%d%H%M%S`
    echo "backup last package complete..."
 fi
-   echo "backup compile package start..."
-   mv /var/lib/jenkins/workspace/gp-back/target/gp-back-*-SNAPSHOT.jar /data/jenkins/backup/gp-back.jar
-   echo "backup compile package complete..."
