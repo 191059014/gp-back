0912cbfe8065914b8ea7dbb8aa9f37cee08102c2
黄彪 <191059014@qq.com>
2019-07-06 20:18:37 +0800

    修改脚本

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-java -server -Xmx4000m -Xms4000m -Xss512k -XX:+DisableExplicitGC \
+java -server -Xmx1024m -Xms2048m -Xss512k -XX:+DisableExplicitGC \
      -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled \
      -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods \
      -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 \
