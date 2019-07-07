00858f54f8fdbfa2013672de7f6ff748525b6400
黄彪 <191059014@qq.com>
2019-07-07 18:39:25 +0800

    修改脚本

--- backup.sh
+++ backup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-file="/data/jenkins/deploy/gp-back.jar"
-if [[ -f "$file" ]]
+fileDir="/data/jenkins/deploy/gp-back"
+if [[ -d "$fileDir" ]]
 then
-   echo "backup last package start..."
-   mv /data/jenkins/deploy/gp-back.jar /data/jenkins/backup/gp-back.jar.`date +%Y%m%d%H%M%S`
-   echo "backup last package complete..."
+   mv /data/jenkins/deploy/gp-back /data/jenkins/backup/gp-back/gp-back.`date +%Y%m%d%H%M%S`
+   echo "backup last package success..."
 fi
