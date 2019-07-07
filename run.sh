46b2fcdc441fef69ab8e50b22ca394600fcfa59e
黄彪 <191059014@qq.com>
2019-07-07 18:58:26 +0800

    修改脚本执行权限

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ java -server -Xmx2048m -Xms1024m -Xss512k -XX:+DisableExplicitGC \
      -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled \
      -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods \
      -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 \
-     -jar gp-back.jar
+     -jar gp-back-*-SNAPSHOT.jar
