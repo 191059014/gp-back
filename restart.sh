a1ded41af4ea75e24a7932105ba41cf1645679eb
黄彪 <191059014@qq.com>
2019-08-14 02:16:10 +0800

    脚本重启

--- restart.sh
+++ restart.sh
@@ -4,12 +4,11 @@ if [ -n '$pid' ]
 then
    echo "stop gp-back..."
    kill -9 $pid
-   echo "start gp-back..."
-   nohup java -jar gp-back-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
-else
-   echo "start gp-back..."
-   nohup java -jar gp-back-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
 fi
+echo "start gp-back..."
+nohup java -jar gp-back-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
+tail -f nohup.out
+
 # linux只能执行格式为unix格式的脚本
 # 因为在dos/window下按一次回车键实际上输入的是“回车（CR)”和“换行（LF）”，而Linux/unix下按一次回车键只输入“换行（LF）”，所以修改的sh文件在每行都会多了一个CR，所以Linux下运行时就会报错找不到命令
 # vim restart.sh
