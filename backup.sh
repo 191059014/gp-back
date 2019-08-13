115a0d170ac2eb7cbbb5022757d821fab77ebd61
黄彪 <191059014@qq.com>
2019-08-14 02:12:09 +0800

    脚本重启

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-fileDir="/data/jenkins/deploy/gp-back"
-if [[ -d "$fileDir" ]]
-then
-   mv /data/jenkins/deploy/gp-back /data/jenkins/backup/gp-back/gp-back.`date +%Y%m%d%H%M%S`
-   echo "backup last package success..."
-fi
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -l
-java -server -Xmx2048m -Xms1024m -Xss512k -XX:+DisableExplicitGC \
-     -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled \
-     -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods \
-     -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 \
-     -jar target/gp-back-*-SNAPSHOT.jar
