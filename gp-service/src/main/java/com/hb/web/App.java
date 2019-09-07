223b9e98e41092589a39a4c698f5f531f29a0b81
Mr.Huang <191059014@qq.com>
2019-09-07 20:15:52 +0800

    开启事务

--- gp-service/src/main/java/com/hb/web/App.java
+++ gp-service/src/main/java/com/hb/web/App.java
@@ -3,12 +3,14 @@ package com.hb.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * web程序入口
  */
 @SpringBootApplication
 @ImportResource({"classpath*:META-INF/applicationContext-web.xml"})
+@EnableTransactionManagement
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
