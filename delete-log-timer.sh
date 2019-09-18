e8ad8b35a34ba1b90d79ad7601607fcc89efcfd5
Mr.Huang <191059014@qq.com>
2019-09-18 19:27:40 +0800

    优化风控

--- delete-log-timer.sh
+++ delete-log-timer.sh
@@ -2,6 +2,6 @@
 cd /data/
 rm -f gpback.out gpweb.out gpbatch.out
 echo `date "+%Y-%m-%d %H:%M:%S"` "delete gpback.out gpweb.out gpbatch.out [path:/data/]" >> /timer/timer.log
-find /log/ -mtime +7 -name "gp*.log*" -exec rm -rf {} \
-echo `date "+%Y-%m-%d %H:%M:%S"` "删除7天前的日志文件 [path:/log/]" >> /timer/timer.log
+find /log/ -mtime +3 -name "gp*.log*" -exec rm -rf {} \
+echo `date "+%Y-%m-%d %H:%M:%S"` "删除3天前的日志文件 [path:/log/]" >> /timer/timer.log
 echo "=======================================" >> /timer/timer.log
