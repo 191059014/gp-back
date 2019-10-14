7a3c9fa8abca36f27eff34a2290313525fe8612c
Mr.Huang <191059014@qq.com>
2019-10-14 20:35:21 +0800

    删除多余

--- delete-log-timer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-find /data/ -mtime +1 -name "*.out" -exec rm -rf {} \;
-echo `date "+%Y-%m-%d %H:%M:%S"` "delete *.out [path:/data/]" >> /timer/timer.log
-find /log/gpbatch/ -mtime +1 -name "*.log" -exec rm -rf {} \;
-echo `date "+%Y-%m-%d %H:%M:%S"` "delete *.log [path:/log/gpbatch/]" >> /timer/timer.log
-echo "=======================================" >> /timer/timer.log
